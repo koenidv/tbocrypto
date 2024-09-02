7c3b4801ba111ef02a6937909b4deb663531dab3
Florian König <koenidv@gmail.com>
2024-09-02 23:57:48 +0200

    ♻ move sharedpreferences methods from sharedprefs cache to super class

--- app/src/main/java/com/koenidv/tbocrypto/data/SharedPrefsCache.kt
+++ app/src/main/java/com/koenidv/tbocrypto/data/SharedPrefsCache.kt
@@ -1,36 +1,24 @@
 package com.koenidv.tbocrypto.data
 
 import android.content.Context
-import android.util.Log
-import com.google.gson.Gson
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Inject
 
-enum class Caches(name: String) {
-    CURRENT("currentPrice"),
-    HISTORIC("historicData"),
-    COINS("coins")
+enum class Caches {
+    CURRENT,
+    HISTORIC,
+    COINS
 }
 
 @ViewModelScoped
 class SharedPrefsCache @Inject constructor(
     @ApplicationContext context: Context
-) : SimpleCacheInterface {
-
-    private val prefs = context.getSharedPreferences("cache", Context.MODE_PRIVATE)
-    private val gson = Gson()
+) : SimpleCacheInterface, SharedPrefs("cache", context) {
 
     override fun updateCurrentPrice(value: CurrentPrice) = set(Caches.CURRENT.name, value)
     override fun updateHistoricData(value: HistoricalData) = set(Caches.HISTORIC.name, value)
     override fun updateCoins(value: List<Coin>) = set(Caches.COINS.name, value)
-    private fun set(key: String, value: Any) {
-        Log.d("SharedPrefsCache", "Setting $key to $value")
-        prefs.edit()
-            .putString(key, gson.toJson(value))
-            .putString("${key}-timestamp", System.currentTimeMillis().toString())
-            .apply()
-    }
 
     override fun getLastCurrentPrice(): CurrentPrice? =
         get(Caches.CURRENT.name, CurrentPrice::class.java) as CurrentPrice?
@@ -42,11 +30,6 @@ class SharedPrefsCache @Inject constructor(
     override fun getLastCoins(): List<Coin>? =
         (get(Caches.COINS.name, Array<Coin>::class.java) as Array<Coin>?)?.toList()
 
-    private fun <T> get(key: String, classType: Class<T>): Any? {
-        val json = prefs.getString(key, null)
-        return gson.fromJson(json, classType)
-    }
-
     override fun getCurrentPriceTimestamp(): Long? = getTimestamp(Caches.CURRENT.name)
     override fun getHistoricDataTimestamp(): Long? = getTimestamp(Caches.HISTORIC.name)
     override fun getCoinsTimestamp(): Long? = getTimestamp(Caches.COINS.name)
@@ -54,5 +37,4 @@ class SharedPrefsCache @Inject constructor(
         return prefs.getString("${key}-timestamp", null)?.toLong()
     }
 
-    override fun clear() = prefs.edit().clear().apply()
 }
